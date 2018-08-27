4945bf1bcbb0b327096bd04f6e1c2c1601ed2254
Unknown <sn-z777@outlook.com>
2018-08-27 10:54:33 +0200

    timer and cellmatrix

---
+++ src/gameoflife/Cell.java
@@ -0,0 +1,29 @@
+package gameoflife;
+
+public class Cell {
+
+	private int xCoordinate;
+	private int yCoordinate;
+	private boolean isAlive;
+
+	public Cell(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+	public int getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+}
--- src/gameoflife/SimulationPanel.java
+++ src/gameoflife/SimulationPanel.java
@@ -2,6 +2,7 @@ package gameoflife;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -10,11 +11,26 @@ public class SimulationPanel extends JPanel{
 
 	private static final int WIDTH = 700;
 	private static final int HEIGHT = 700;
+	private Cell[][] cellMatrix;
 
 	public SimulationPanel() {
 		super();
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setBackground(Color.GRAY);
 		setLayout(null);
+		cellMatrix = new Cell[100][100];
+
+		// instantiate Cells
+		for(int xPos = 0; xPos < cellMatrix.length; xPos++) {
+			for(int yPos = 0; xPos < cellMatrix[0].length; yPos++) {
+				cellMatrix[xPos][yPos] = new Cell(xPos, yPos);
+			}
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
 	}
 }
