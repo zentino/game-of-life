0442bd4a9da7339b86c0da37689a574c03ef816a
Unknown <sn-z777@outlook.com>
2018-09-19 18:35:10 +0200

    removed unused member variables

--- src/gameoflife/Cell.java
+++ src/gameoflife/Cell.java
@@ -2,8 +2,6 @@ package gameoflife;
 
 public class Cell {
 
-	private int xCoordinate;
-	private int yCoordinate;
 	private boolean isAlive;
 	private boolean isAliveNextGen;
 	private int neighbours;
@@ -14,17 +12,9 @@ public class Cell {
 		this.neighbours = 0;
 	}
 
-	public int getxCoordinate() { return xCoordinate; }
-
-	public void setxCoordinate(int xCoordinate) { this.xCoordinate = xCoordinate; }
-
 	public boolean isAlive() { return isAlive; }
 
 	public void setAlive(boolean isAlive) { this.isAlive = isAlive; }
-
-	public int getyCoordinate() { return yCoordinate; }
-
-	public void setyCoordinate(int yCoordinate) { this.yCoordinate = yCoordinate; }
 
 	public boolean isAliveNextGen() { return isAliveNextGen; }
 
